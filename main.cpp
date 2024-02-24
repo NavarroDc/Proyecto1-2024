ba468d9f7e5f4c240bf3deb646e738df6f0b959a
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2024-02-24 09:57:04 -0600

    caso 1 listo

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int opcionElegida;
     int cedula;
     string nombreCompleto;
@@ -19,8 +18,7 @@ int main()
 
     setlocale(LC_ALL, "");
 
-    do
-    {
+    do {
         cout<<"Cálculos de acuerdo al peso de la persona."<<endl;
         cout<<" "<<endl;
         cout<<"1. Ingresar datos de la persona."<<endl;
@@ -35,38 +33,31 @@ int main()
         system("CLS");
         bool opcionErronea = cin.bad() || !(opcionElegida >= 1 && opcionElegida <= 5);
 
-        if(opcionErronea)
-        {
+        if(opcionErronea) {
             cout<<"Opción inválida, vuelva a intentarlo. Debe escribir un número del 1 al 5."<<endl;
             cin.clear();
             cin.ignore();
             system("Pause");
             system("CLS");
-        }
-        else
-        {
-            switch(opcionElegida)
-            {
+        } else {
+            switch(opcionElegida) {
 
             //CASO 1 PEDIR CEDULA Y NOMBRE
-            case 1:
-            {
+            case 1: {
                 char espacio = ' ';
-
                 cout<<"Ingrese su número de cédula."<<endl;
-                cin.clear();
-                cin.ignore();
-                cin>>cedula;
-
                 // Preguntar hasta que la cédula sea un número
                 do {
+                    cedula = 0;
+                    cin.clear();
+                    cin.ignore();
+                    cin>>cedula;
                     // ¿ la cédula son sólo números entre 1 a 9 dígitos ?
                     if(cedula >= 1 && cedula <= 999999999) {
                         cedulaValida = true;
-                    }
-                    else {
+                    } else {
                         cedulaValida = false;
-                        cedula = 0;
+
                         cout<<"Sólo puede ingresar números en el apartado de la cédula."<<endl;
                         cout<<" "<<endl;
                         cout<<"Digite de nuevo la cédula."<<endl;
@@ -74,7 +65,7 @@ int main()
                         cin.ignore();
                         cin>>cedula;
                     }
-                // ¿ La cédula es inválida ?
+                    // ¿ La cédula es inválida ?
                 } while(cedulaValida == false);
 
                 cout<<"Ingrese su nombre con dos apellidos."<<endl;
@@ -83,7 +74,8 @@ int main()
                 do {
                     cin.clear();
                     cin.ignore();
-                    cin>>nombreCompleto;
+                    getline (cin,nombreCompleto);
+
                     int contadorEspacios = 0;
                     //Loop for recorre lo que hay en nombreCompleto
                     for (char i : nombreCompleto) {
@@ -97,14 +89,14 @@ int main()
                     //¿ la cantidad de espacio es diferente de 2 ?
                     if (contadorEspacios != 2) {
                         nombreValido = false;
+                        cout << "Error, el nombre debe estar compuesto por 3 palabras y 2 espacios. Nombre ingresado:" << nombreCompleto << endl;
+                        cout << " " << endl;
+                        cout << "Ingrese de nuevo su nombre."<< endl;
                         nombreCompleto = ""; // limpiar el nombre para vaidarlo de nuevo
-                        cout << "Debe ingresar únicamente su nombre con dos apellidos. Nombre ingresado:" << nombreCompleto << endl;
-                    }
-                    else {
+                    } else {
                         nombreValido = true;
                     }
-
-                //¿El nombre es inválido? Si es inválido, se repite el lopp (do)
+                    //¿El nombre es inválido? Si es inválido, se repite el lopp (do)
                 } while(nombreValido == false);
 
 
@@ -115,8 +107,7 @@ int main()
             }
 
         }
-    }
-    while(salir = false);
+    } while(salir = false);
 
 
     return 0;
