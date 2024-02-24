4122403872634beca645870dbfbbedeba329918c
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2024-02-24 09:33:46 -0600

    case 1 casi listo

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ int main()
             switch(opcionElegida)
             {
 
-//CASO 1 PEDIR CEDULA Y NOMBRE
+            //CASO 1 PEDIR CEDULA Y NOMBRE
             case 1:
             {
                 char espacio = ' ';
@@ -59,15 +59,14 @@ int main()
                 cin.ignore();
                 cin>>cedula;
 
-                do
-                {
-                    if(cedula >= 1 && cedula <= 999999999)
-                    {
+                // Preguntar hasta que la cédula sea un número
+                do {
+                    // ¿ la cédula son sólo números entre 1 a 9 dígitos ?
+                    if(cedula >= 1 && cedula <= 999999999) {
                         cedulaValida = true;
-
                     }
-                    else
-                    {   cedulaValida = false;
+                    else {
+                        cedulaValida = false;
                         cout<<"Sólo puede ingresar números en el apartado de la cédula."<<endl;
                         cout<<" "<<endl;
                         cout<<"Digite de nuevo la cédula."<<endl;
@@ -75,32 +74,35 @@ int main()
                         cin.ignore();
                         cin>>cedula;
                     }
-
-
-                }
-                while(cedulaValida == false);
-
-
-
+                // ¿ La cédula es inválida ?
+                } while(cedulaValida == false);
 
                 cout<<"Ingrese su nombre con dos apellidos."<<endl;
-                cin>>nombreCompleto;
-                for (char i : nombreCompleto)
-                    if (i == espacio)
-                    {
-                        nombreValido = true;
-                        contadorEspacios++;
-                    }
 
-                do
-                {
-                    if (contadorEspacios != 2)
-                    {
+                // Preguntarle al usuario hassta que el nombre sea válido
+                do {
+                    cin>>nombreCompleto;
+                    //Loop for recorre lo que hay en nombreCompleto
+                    for (char i : nombreCompleto) {
+                        //Condicional if indica que se encontró un espacio
+                        // ¿ es el caracter actual (i) un espacio ?
+                        if (i == espacio) {
+                            contadorEspacios++;//Aumenta el contador en caso de que sí
+                        }
+                    }
+                    //Condición de que los espacios en el string tenga que ser 2
+                    //¿ la cantidad de espacio es diferente de 2 ?
+                    if (contadorEspacios != 2) {
                         nombreValido = false;
                         cout << "Debe ingresar únicamente su nombre con dos apellidos." << endl;
                     }
-                }
-                while(nombreValido = false);
+                    else {
+                        nombreValido = true;
+                    }
+
+                //¿El nombre es inválido? Si es inválido, se repite el lopp (do)
+                } while(nombreValido == false);
+
 
                 break;
             }
